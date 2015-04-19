4225540ef52a7edb61e212d37d2329bd3a92b541
Kittitat Rodchaidee <rodch100@gmail.com>
2015-04-18 21:42:42 -0700

    photo edit no drawing

--- photoedit/www/js/photoedit_controller.js
+++ photoedit/www/js/photoedit_controller.js
@@ -1,55 +1,5 @@
 angular.module('starter.controllers', ['ionic'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $location) {
-    // Form data for the login modal
-    $scope.loginData = {};
-
-    // Create the login modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-        scope: $scope
-    }).then(function(modal) {
-        $scope.modal = modal;
-    });
-
-    // Triggered in the login modal to close it
-    $scope.closeLogin = function() {
-        $scope.modal.hide();
-    };
-
-    // Open the login modal
-    $scope.login = function() {
-        $scope.modal.show();
-    };
-
-    // Perform the login action when the user submits the login form
-    $scope.doLogin = function() {
-        console.log('Doing login', $scope.loginData);
-
-        // Simulate a login delay. Remove this and replace with your login
-        // code if using a login system
-        $timeout(function() {
-            $scope.closeLogin();
-        }, 1000);
-    };
-
-    $scope.goToTakePhoto = function() {
-        $location.path('/app/edit_take');
-
-    }
-
-    $scope.goSelectPhoto = function() {
-        $location.path('/app/edit_select');
-    }
-
-    $scope.goEditPhoto = function() {
-        $location.path('/app/meesh_photo_edit');
-    }
-
-    $scope.settings = {
-        enableFriends: true
-    };
-})
-
 .controller('PhotoEditCtrl', function($scope, $ionicSideMenuDelegate) {
     $scope.brightnessValue = 10;
     $scope.decreaseBrightness = -10;
@@ -268,27 +218,3 @@ angular.module('starter.controllers', ['ionic'])
 
     })
 })
-
-.controller('PlaylistsCtrl', function($scope) {
-    $scope.playlists = [{
-        title: 'Reggae',
-        id: 1
-    }, {
-        title: 'Chill',
-        id: 2
-    }, {
-        title: 'Dubstep',
-        id: 3
-    }, {
-        title: 'Indie',
-        id: 4
-    }, {
-        title: 'Rap',
-        id: 5
-    }, {
-        title: 'Cowbell',
-        id: 6
-    }];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {});
